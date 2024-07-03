a2dc6dd71b0974f2067d4ebaea9bf4a8f62290e6
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-07-03 18:59:50 +0800

    another file

--- test.py
+++ test.py
@@ -39,31 +39,59 @@ def fcn_phi3(k):
         return phi3[k-1] + eta * (y3[k] - y3[k-1] - (phi3[k-1] * y3[k-1] - y3[k-2]) - phi3[k-1] * (umk3[k-1] - umk3[k-2])) * (y3[k-1] - y3[k-2]) / (mu + abs(y3[k-1]-y3[k-2])**2 + abs(umk3[k-1]-umk3[k-2])**2)
 
 def fcn_umk1(k):
-    return umk1[k-1] + (rho * (phi2[k].T * (yd1[k+1]) - y1[k] - phi1[k] * y1[k] - y1[k-1]))
+    if k < n:
+        return umk1[k-1] + (rho * (phi2[k].T * (yd1[k+1]) - y1[k] - phi1[k] * y1[k] - y1[k-1]))
+    else:
+        return umk1[k-1]
 
 def fcn_umk2(k):
-    return umk2[k-1] + (rho * (phi2[k].T * (yd2[k+1]) - y2[k] - phi1[k] * y2[k] - y2[k-1]))
+    if k < n:
+        return umk2[k-1] + (rho * (phi2[k].T * (yd2[k+1]) - y2[k] - phi1[k] * y2[k] - y2[k-1]))
+    else:
+        return umk2[k-1]
 
 def fcn_umk3(k):
-    return umk3[k-1] + (rho * (phi2[k].T * (yd3[k+1]) - y3[k] - phi3[k] * y3[k] - y3[k-1]))
+    if k < n:
+        return umk3[k-1] + (rho * (phi2[k].T * (yd3[k+1]) - y3[k] - phi3[k] * y3[k] - y3[k-1]))
+    else:
+        return umk3[k-1]
 
 def fcn_usk1(k):
-    return phi2[k]**-1 * (yd1[k+1]) - phi1[k] * y1[k] - y1[k-1] - y1[k] - (1 - 1*T) * yd1[k] - y1[k] + epsilon * T * np.sign(yd1[k] - y1[k])
+    if k < n:
+        return (1 / phi2[k][0] if phi2[k][0] != 0 else 0) * (yd1[k+1]) - phi1[k][0] * y1[k][0] - y1[k-1][0] - y1[k][0] - (1 - 1*T) * yd1[k] - y1[k][0] + epsilon * T * np.sign(yd1[k] - y1[k][0])
+    else:
+        return usk1[k-1]
 
 def fcn_usk2(k):
-    return phi2[k]**-1 * (yd2[k+1]) - phi1[k] * y2[k] - y2[k-1] - y2[k] - (1 - 1*T) * yd2[k] - y2[k] + epsilon * T * np.sign(yd2[k] - y2[k])
+    if k < n:
+        return (1 / phi2[k][0] if phi2[k][0] != 0 else 0) * (yd2[k+1]) - phi1[k][0] * y2[k][0] - y2[k-1][0] - y2[k][0] - (1 - 1*T) * yd2[k] - y2[k][0] + epsilon * T * np.sign(yd2[k] - y2[k][0])
+    else:
+        return usk2[k-1]
 
 def fcn_usk3(k):
-    return phi2[k]**-1 * (yd3[k+1]) - phi1[k] * y3[k] - y3[k-1] - y3[k] - (1 - 1*T) * yd3[k] - y3[k] + epsilon * T * np.sign(yd3[k] - y3[k])
+    if k < n:
+        return (1 / phi2[k][0] if phi2[k][0] != 0 else 0) * (yd3[k+1]) - phi1[k][0] * y3[k][0] - y3[k-1][0] - y3[k][0] - (1 - 1*T) * yd3[k] - y3[k][0] + epsilon * T * np.sign(yd3[k] - y3[k][0])
+    else:
+        return usk3[k-1]
 
 def fcn_u1():
-    return umk1 + gamma * usk1
+    return umk1 + gamma @ usk1
 
 def fcn_u2():
-    return umk2 + gamma * usk2
+    return umk2 + gamma @ usk2
 
 def fcn_u3():
-    return umk3 + gamma * usk3
+    return umk3 + gamma @ usk3
+
+def fcn_y1(k):
+    
+    return u1[k][0] + np.sin(y1[k][0])
+    
+def fcn_y2(k):
+    return u2[k][0] + np.sin(y2[k][0])
+
+def fcn_y3(k):
+    return u3[k][0] + np.sin(y3[k][0])
 
 # Parameters
 # Step Factor Initializations
@@ -84,26 +112,31 @@ yd1 = np.zeros(n + 1)
 yd2 = np.zeros(n + 1)
 yd3 = np.zeros(n + 1)
 
-#Initialize umk
+# Initialize umk
 umk1 = np.zeros(n + 1)
 umk2 = np.zeros(n + 1)
 umk3 = np.zeros(n + 1)
+
+# Initialize u
+u1 = np.zeros((n, 1))
+u2 = np.zeros((n, 1))
+u3 = np.zeros((n, 1))
 
 usk1 = np.zeros(n + 1)
 usk2 = np.zeros(n + 1)
 usk3 = np.zeros(n + 1)
 
 # Define phi as arrays
-phi1 = np.zeros((n + 1, 1))
-phi2 = np.zeros((n + 1, 1))
-phi3 = np.zeros((n + 1, 1))
+phi1 = np.zeros((n, 1))
+phi2 = np.zeros((n, 1))
+phi3 = np.zeros((n, 1))
 
 # Define y as arrays
 y1 = np.zeros((n + 1, 1))
 y2 = np.zeros((n + 1, 1))
 y3 = np.zeros((n + 1, 1))
 
-for k in range(n + 1):
+for k in range(n):
     yd1[k] = fcn_yd1(k)
     yd2[k] = fcn_yd2(k)
     yd3[k] = fcn_yd3(k)
@@ -115,12 +148,23 @@ for k in range(n + 1):
     umk1[k] = fcn_umk1(k)
     umk2[k] = fcn_umk2(k)
     umk3[k] = fcn_umk3(k)
+    
+    usk1[k] = fcn_usk1(k)
+    usk2[k] = fcn_usk2(k)
+    usk3[k] = fcn_usk3(k)
+    
+    
+    
+    y1[k+1] = fcn_y1(k)
+    y2[k+1] = fcn_y2(k)
+    y3[k+1] = fcn_y3(k)
 
 # Plot results using subplots
 fig, axs = plt.subplots(3, 1, figsize=(10, 8))
 
 # Plot yd1
 axs[0].plot(yd1, 'k-', linewidth=1.5, label='$y_d1(k)$')
+axs[0].plot(y1, 'k-', linewidth=1.5, label='$y_d1(k)$')
 axs[0].set_xlabel('k')
 axs[0].set_ylabel('$y_d1$')
 axs[0].set_title('yd1 over iterations')
@@ -129,6 +173,7 @@ axs[0].grid(True)
 
 # Plot yd2
 axs[1].plot(yd2, 'r-', linewidth=1.5, label='$y_d2(k)$')
+axs[1].plot(y2, 'r-', linewidth=1.5, label='$y_d2(k)$')
 axs[1].set_xlabel('k')
 axs[1].set_ylabel('$y_d2$')
 axs[1].set_title('yd2 over iterations')
